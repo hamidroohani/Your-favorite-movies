8e2d9a5a044c325ff269f6289e6e6e9deaefb9b3
HamidRoohani <hamid.dev@yahoo.com>
2021-08-14 12:59:08 +0430

    add auth ; add tables for meta and favorites

---
+++ database/migrations/2021_05_25_112603_create_user_meta_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserMetaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_meta', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('key');
+            $table->text('value');
+            $table->timestamps();
+
+            $table->unique(['user_id','key']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_meta');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 
 Route::get('/my-favorite-movies',[\App\Http\Controllers\MoviesController::class,'my_favorite_movies'])->name('my-favorite-movies');
 Route::post('/sync_movies',[\App\Http\Controllers\MoviesController::class,'sync_movies'])->name('sync_movies');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
