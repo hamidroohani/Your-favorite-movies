5b7cd7270a9403f475ff4366a74ed995a88b9ec3
HamidRoohani <hamid.dev@yahoo.com>
2021-08-14 15:27:58 +0430

    Update MoviesController.php

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Favorite;
+use App\Models\User;
 use App\Models\UserMeta;
 use Illuminate\Http\Request;
 
@@ -9,11 +11,46 @@ class MoviesController extends Controller
 {
     public function my_favorite_movies()
     {
-        return view("my_favorite_movies");
+        $user = User::with('Metas')->find(auth()->id());
+        $metas = $user->Metas;
+        return view("my_favorite_movies", compact('metas'));
     }
 
     public function sync_movies(Request $request)
     {
+        // return if was not auth
+
+        $user_id = auth()->id();
+
+        $web_favorites = $request->input('ids');
+        $db_favorites = Favorite::where("user_id", $user_id)->get()->toArray();
+
+        foreach ($db_favorites as $db_favorite) {
+            if (!in_array($db_favorite->movie_id, $web_favorites)) {
+                $db_favorite->delete();
+            }
+        }
+
+        foreach ($web_favorites as $web_favorite) {
+            if (!in_array($web_favorite, $db_favorites)) {
+                $record = new Favorite();
+                $record->user_id = $user_id;
+                $record->movie_id = $web_favorite;
+                $record->save();
+            }
+        }
+
+        $meta = UserMeta::where("user_id", $user_id)->where("key", "favorite_movies")->first();
+        if ($meta) {
+            $meta->value = serialize($web_favorites);
+            $meta->save();
+        } else {
+            $meta = new UserMeta();
+            $meta->user_id = $user_id;
+            $meta->key = 'favorite_movies';
+            $meta->value = serialize($web_favorites);
+            $meta->save();
+        }
         return $request->input('ids');
     }
 }
